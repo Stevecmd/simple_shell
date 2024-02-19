d369428929930da9c5e970453dc7a51b7bfe9a98
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2024-02-20 01:53:04 +0300

    fork final update

--- fork.c
+++ fork.c
@@ -14,17 +14,26 @@ int main(void)
     pid_t my_pid;
     pid_t pid;
 
-    printf("Before fork\n");
+    my_pid = getpid();
+
+    printf("Before fork:\n");
     pid = fork();
-    if (pid == -1)
+    printf("My pid is %u\n", pid);
+
+    if(pid == -1)
     {
         perror("Error:");
         return (1);
     }
-    printf("After fork\n");
-    my_pid = getpid();
-    printf("My pid is %u\n", my_pid);
-    return (0);
-}
+
+    if(pid == 0)
+    {
+        printf("After fork:\n");
+        my_pid = getpid();
+        printf("My pid is %u\n", my_pid);
+    }
+
+    return 0;
+    }
 
 #endif /* _FORK_ */
