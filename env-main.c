1326b22275d530efa5e82a411810e5432511d123
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2024-02-20 00:34:03 +0300

    modifications to env-main for it to work

--- env-main.c
+++ env-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the environment
+ * print_environment - prints the environment
  *
  * Return: Always 0.
  */
-int main(int ac, char **av, char **env)
+int print_environment(int ac, char **av, char **env)
 {
     unsigned int i;
 
