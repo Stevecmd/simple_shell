9e95fc96ef2a29adcd811c620cbe2fb4a9aac461
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2024-02-20 01:36:40 +0300

    fork reset

--- fork.c
+++ fork.c
@@ -14,30 +14,16 @@ int main(void)
     pid_t my_pid;
     pid_t pid;
 
-    printf("Before fork:\n");
-    my_pid = getpid();
-    printf("My pid is %u\n", my_pid);
-
+    printf("Before fork\n");
     pid = fork();
-
-    if (pid == 0)
-    {
-        printf("After fork:\n");
-        my_pid = getpid();
-        printf("My pid is %u\n", my_pid);
-        return (0);
-    }
-
-    else if (pid == -1)
+    if (pid == -1)
     {
         perror("Error:");
         return (1);
     }
-    else
-    {
-
-    }
-
+    printf("After fork\n");
+    my_pid = getpid();
+    printf("My pid is %u\n", my_pid);
     return (0);
 }
 
