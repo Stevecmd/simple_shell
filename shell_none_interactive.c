e8fc50aae3313cfdddeec32e20a9a2421cdcac99
Stevecmd <murimi101@gmail.com>
2024-02-27 16:23:47 +0300

    Shell has passed betty checks

--- shell_none_interactive.c
+++ shell_none_interactive.c
@@ -39,39 +39,49 @@ void shell_none_interactive(void)
 
 	/* Read input from stdin until EOF */
 	ssize_t bytes_read;
-	while ((bytes_read = read(STDIN_FILENO, line, sizeof(line))) > 0) {
+
+	while ((bytes_read = read(STDIN_FILENO, line, sizeof(line))) > 0)
+	{
 		/* Tokenize input line into arguments */
 		char *token = strtok(line, " \t\n");
 		int argc = 0;
-		while (token && argc < MAX_ARGS) {
+
+		while (token && argc < MAX_ARGS)
+		{
 			args[argc++] = token;
 			token = strtok(NULL, " \t\n");
 		}
 		args[argc] = NULL; /* Set the last argument to NULL as required by execvp */
 
-		if (argc == 0) {
+		if (argc == 0)
+		{
 			continue; /* Skip empty lines */
 		}
 
 		/* Fork a child process to execute the command */
 		pid = fork();
-		if (pid < 0) {
+		if (pid < 0)
+		{
 			perror("fork");
 			exit(EXIT_FAILURE);
-		} else if (pid == 0) {
+		} else if (pid == 0)
+		{
 			/* Child process */
-			if (execvp(args[0], args) == -1) {
+			if (execvp(args[0], args) == -1)
+			{
 				perror("execvp"); /* Print error if execution fails */
 				exit(EXIT_FAILURE);
 			}
 			/* No need to free memory in child process */
-		} else {
+		} else
+		{
 			/* Parent process */
 			wait(NULL); /* Wait for the child process to complete */
 		}
 	}
 
-	if (bytes_read == -1) {
+	if (bytes_read == -1)
+	{
 		perror("read");
 		exit(EXIT_FAILURE);
 	}
